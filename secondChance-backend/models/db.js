356277fc4704e4fc516011dfac2693b1309b144a
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-09-07 19:33:39 +0800

    fix issue - call function

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -16,7 +16,7 @@ async function connectToDatabase() {
   const client = new MongoClient(url);
 
   // Task 1: Connect to MongoDB
-  await client.connect;
+  await client.connect();
 
   // Task 2: Connect to database giftDB and store in variable dbInstance
   dbInstance = client.db(dbName);
